7c7ea7610d49fe7839d17698a9f948f4d7cc5e18
jguida <joeguida@outlook.com>
2025-07-20 19:29:13 -0700

    add lsp

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,12 @@
 return{
+    {
+        'neovim/nvim-lspconfig',
+        event = 'BufReadPre',
+        config = function()
+            vim.lsp.enable('clangd')
+            vim.lsp.enable('cmake')
+        end
+    },
     { 
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
@@ -23,6 +31,7 @@ return{
     },
 	{ 
         'nvim-lualine/lualine.nvim', 
+        deps = { 'kyazdani42/nvim-web-devicons' },
         opts = { theme = 'palenight' },
         lazy = false
     },
@@ -46,6 +55,31 @@ return{
         event = 'BufReadPre',
         config = function()
             require('colorizer').setup()
+        end
+    },
+    {
+        'tpope/vim-abolish',
+        event = 'BufReadPre'
+    },
+    {
+        'hrsh7th/nvim-cmp',
+        deps = { 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+        event = 'BufReadPre',
+        config = function()
+            local cmp = require('cmp')
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body)
+                    end
+                },
+                window = {
+                    completion = cmp.config.window.bordered()
+                },
+                sources = cmp.config.sources({
+                    { name = 'luasnip' }
+                })
+            }) 
         end
     }
 }
