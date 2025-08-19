e06f7c733be93809e14e9eb9e87a5aaed0612860
jguida <joeguida@outlook.com>
2025-08-18 20:02:44 -0700

    add lspconfig for clangd

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -7,6 +7,47 @@ return{
         'catppuccin/nvim',
         lazy = false
     },
+    {
+        'hrsh7th/nvim-cmp',
+        event = 'InsertEnter',
+        dependencies = {
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-nvim-lsp',
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip'
+        },
+        config = function()
+            local cmp = require('cmp')
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body)
+                    end
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-n>'] = cmp.mapping.select_next_item(),
+                    ['<C-p>'] = cmp.mapping.select_prev_item(),
+                    ['<CR>'] = cmp.mapping.confirm({ selct = true })
+                }),
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'buffer' },
+                    { name = 'luasnip' }
+                }),
+            })
+        end     
+    },
+    {
+        'neovim/nvim-lspconfig',
+        event = { 'BufReadPre', 'BufNewFile' },
+        config = function()
+            require('lspconfig').clangd.setup {
+                cmd = { 'clangd', '--background-index' },
+                filetypes = { 'c', 'cpp' },
+                capabilities = require('cmp_nvim_lsp').default_capabilities()
+            }
+        end
+    },
     { 
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
