c460e3e7c33b040e658764309f41ce64b4fecf11
jguida <joeguida@outlook.com>
2025-08-20 21:26:56 -0700

    supress update output, add fresh install script to get packages from winget

---
+++ fresh_install.ps1
@@ -0,0 +1,3 @@
+winget install --id Neovim.Neovim
+winget install --id JanDeDobbeleer.OhMyPosh
+. './update_dotfiles.ps1'
--- update_dotfiles.ps1
+++ update_dotfiles.ps1
@@ -1,7 +1,7 @@
 # Neovim
-Remove-Item -Recurse -Force $Env:LOCALAPPDATA/nvim
-Copy-Item -Recurse -Force "$PSScriptRoot/nvim" -Destination $Env:LOCALAPPDATA 
+Remove-Item -Recurse -Force $Env:LOCALAPPDATA/nvim | Out-Null
+Copy-Item -Recurse -Force "$PSScriptRoot/nvim" -Destination $Env:LOCALAPPDATA | Out-Null
 
 # PowerShell
-Remove-Item -Recurse -Force "$HOME/Documents/PowerShell"
-Copy-Item -Recurse -Force "$PSScriptRoot/PowerShell" -Destination "$HOME/Documents"
+Remove-Item -Recurse -Force "$HOME/Documents/PowerShell" | Out-Null
+Copy-Item -Recurse -Force "$PSScriptRoot/PowerShell" -Destination "$HOME/Documents" | Out-Null
