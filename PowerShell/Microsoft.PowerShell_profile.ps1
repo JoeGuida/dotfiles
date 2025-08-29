b5580b5244660a1f34c9177685200c880d18f48b
jguida <joeguida@outlook.com>
2025-08-29 02:01:32 -0700

    add Watch-File to powershell profile

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -36,6 +36,16 @@ function v {
     nvim $file
 }
 
+function Watch-File {
+    param([string]$file)
+
+    while($true) {
+        Clear-Host
+        Get-Content $file
+        Start-Sleep -Seconds 2
+    }
+}
+
 Set-PSReadLineOption -EditMode Vi
 
 oh-my-posh init pwsh --config "$POSH_DIR/themes/multiverse-neon.omp.json"| Invoke-Expression
