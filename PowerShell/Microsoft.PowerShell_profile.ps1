0c3dccfc46b4a83645b696703585c7efe1ef685a
Joe Guida <joeguida@outlook.com>
2026-05-07 13:26:13 -0700

    remove update script, rename powershell profile

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,12 @@
-. $HOME/Documents/PowerShell/profile.ps1
+$pwsh = "$HOME/Documents/PowerShell"
+$PROFILE = Join-Path $pwsh profile.ps1
+
+Get-ChildItem -Path $pwsh/Scripts -Filter '*.ps1' | % {
+    Import-Module $_.FullName -Force
+}
+
+Set-PSReadLineOption -EditMode Vi
+
+devshell
+oh-my-posh -c $posh/themes/craver.omp.json init pwsh | Invoke-Expression
+
